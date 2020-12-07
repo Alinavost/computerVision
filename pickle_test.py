a5b2de0dc86a6fa48b485ea91c0f8654006ad697
Alinavost <alina.vostrikov@gmail.com>
2020-12-07 20:55:11 +0200

    Added Kmeans function - need to check it's working properly

--- pickle_test.py
+++ pickle_test.py
@@ -2,8 +2,13 @@ import cv2
 import os
 import pickle
 import numpy as np
-# import sklearn.model_selection as sk
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.svm import SVC
+import pandas as pd
+
+
+
 
 
 def pickle_caltech101_images(data_path, pickle_path):
@@ -48,7 +53,7 @@ def pickle_caltech101_images(data_path, pickle_path):
     #     print(f"{vals[0].shape},\t{vals[1].shape}")
 
     file = open(pickle_path, 'wb')
-    pickle.dump(dataDict, file, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(dataDict, file, protocol=2)
     file.close()
     print("finished pickling")
 
@@ -86,14 +91,55 @@ def create_sets(data_dict):
 
     return trainSetDict, testSetDict
 
+def GetDefaultParameters(): #too add more parameters
+    class_indices = [10,11,12,13,14,15,16,17,18,19]
+    image_size = (150,150)
+    split = 0.2
+    clusters = 40
+    svm_c = 200
+    degree = 3
+    kernel = 'rbf'
+    gamma = 5
+    step_size = 6
+    bins = clusters
+    validate = False
+    parameters = {"class_indices":class_indices,"validate":validate,"image_size":image_size,"Split":split,"clusters":clusters,"step_size":step_size,"bins":bins, "svm_c":svm_c,"kernel":kernel,"gamma":gamma,'degree':degree}
+    return parameters
+
+def train_kmeans(data, params):
+    sift_vec = []  # define a list of sift
+    for img in data:
+        sift = cv2.xfeatures2d.SIFT_create() #create sifts
+        step_size = params['step_size'] #use the step size as defined in params
+        kp = [cv2.KeyPoint(x, y, step_size) for y in range(0, data[img][0].shape[0], step_size) for x in
+              range(0, data[img][0].shape[1], step_size)] # compute key points
+        points, sifts = sift.compute(np.array(data[img][0]), kp) #compute sifts from key points
+        sift_vec.append(sifts) # add the sift to the sifts list
+
+    # transfer the list to np.array
+    all_sifts_array = list(sift_vec[0])
+    for value in sift_vec[1:]:
+        all_sifts_array = np.append(all_sifts_array, value, axis=0)
+    # compute and return k_means
+    model = MiniBatchKMeans(n_clusters=params["clusters"], random_state=42,batch_size=params['clusters']*4)
+    kmeans = model.fit(all_sifts_array)
+    print("Kmeans trained")
+
+    return kmeans
+
+
+
 
 if __name__ == '__main__':
     data_path = r"C:\Users\Alina\OneDrive\Desktop\Studies\Learning, representation, and Computer Vision\Homework\Task 1\101_ObjectCategories"
     pickle_file_path = r'C:\Users\Alina\PycharmProjects\CVCourseT1\data.pkl'
 
-    # pickle_caltech101_images(data_path, pickle_file_path)
+    #pickle_caltech101_images(data_path, pickle_file_path)
 
     dataDict = load_data(pickle_file_path)
 
     train_set_dict, test_set_dict = create_sets(dataDict)
 
+    params = GetDefaultParameters()
+
+    kmeans_model = train_kmeans(train_set_dict, params)
