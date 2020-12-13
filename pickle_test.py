641396949c649ff1b845cdf96a2b0f6274040b4b
Doraz1 <razdor1@gmail.com>
2020-12-13 20:12:41 +0200

    Updated the dictionary handling for all methods except prepare

--- pickle_test.py
+++ pickle_test.py
@@ -8,7 +8,40 @@ from sklearn.svm import SVC
 import pandas as pd
 
 
-def pickle_caltech101_images(data_path, pickle_path):
+def GetDefaultParameters():  # to add more parameters
+    # data_path = r"C:\Users\Alina\OneDrive\Desktop\Studies\Learning, representation, and Computer Vision\Homework\Task 1\101_ObjectCategories"
+    # pickle_file_path = r'C:\Users\Alina\PycharmProjects\CVCourseT1\data.pkl'
+    data_path = r"C:\Users\razdo\Documents\_Dor\Second Degree documents\Courses\Semester 1\Learning, representation, and Computer Vision\Homework\Task 1 misc\101_ObjectCategories"
+    pickle_file_path = r"C:\Users\razdo\Documents\_Dor\Second Degree documents\Courses\Semester 1\Learning, representation, and Computer Vision\Homework\Task 1 misc\data.pkl"
+
+    class_indices = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    image_size = (150, 150)
+    split = 0.2
+    clusters = 40
+    svm_c = 200
+    degree = 3
+    kernel = 'rbf'
+    gamma = 5
+    step_size = 6
+    bins = clusters
+    validate = False
+    parameters = {"Data": {'CalTechData': data_path, 'PickleData': pickle_file_path},
+                  "Prepare": {1},
+                  "Train": {"step_size": step_size, "clusters": clusters},
+                  "Split": split,
+                  "class_indices": class_indices,
+                  "validate": validate,
+                  "image_size": image_size,
+                  "bins": bins,
+                  "svm_c": svm_c,
+                  "kernel": kernel,
+                  "gamma": gamma,
+                  'degree': degree}
+
+    return parameters
+
+
+def pickle_caltech101_images(dataParams):
     """ Saves images and labels in pickle file for quick loading
 
         loads the images
@@ -21,6 +54,8 @@ def pickle_caltech101_images(data_path, pickle_path):
     print("starting pickling")
 
     # region variables and constants
+    data_path = dataParams['CalTechData']
+    pickle_path = dataParams['PickleData']
     top_folder = os.listdir(data_path)
 
     picnum = 0
@@ -28,7 +63,7 @@ def pickle_caltech101_images(data_path, pickle_path):
     SIZE = 250  # new size
     data = []  # should be SIZE x SIZE x picnum
     labels = []
-    dataDict = {}  # should be 101 X SIZE x SIZE x picnum
+    dataDict = {"Data": {}, "Labels": {}}  # should be 101 X SIZE x SIZE x picnum
     i = 0
     # endregion
 
@@ -48,7 +83,8 @@ def pickle_caltech101_images(data_path, pickle_path):
             if picnum == min(len(pic_list), N):
                 data_array = np.array(data)
                 labels_array = np.array(labels)
-                dataDict[f"{i}"] = (data_array, labels_array)
+                dataDict["Data"][i] = data_array
+                dataDict["Labels"][i] = labels_array
                 i += 1
                 picnum = 0
                 data = []
@@ -59,12 +95,15 @@ def pickle_caltech101_images(data_path, pickle_path):
     #     print(f"{vals[0].shape},\t{vals[1].shape}")
 
     file = open(pickle_path, 'wb')
-    pickle.dump(dataDict, file, protocol=2)
+    pickle.dump(dataDict, file, protocol=2) # dictionary is of type { Data: {1: data1, 2: data2, ...},
+                                                                   #  Labels: {1: labels1, 2: labels2, ...} }
     file.close()
     print("finished pickling")
 
 
-def load_data(pickle_path):
+def GetData(params):
+    pickle_path = params['PickleData']
+
     file = open(pickle_path, 'rb')
     dataDict = pickle.load(file)
     file.close()
@@ -72,81 +111,33 @@ def load_data(pickle_path):
     return dataDict
 
 
-def create_sets(data_dict):
+def TrainTestSplit(dataDict, labelDict, splitParams):
     """ Splits the data from the dictionary to train sets and test sets
 
-        each class has between 32 and 50 images
-        the train set and test set are each half of the total number of images
+        each class has between 32 and 50 images_list
+        the train set and test set are each half of the total number of images_list
         returns a dictionary of the split data sets
 
         """
+    images_list = []
+    labels_list = []
 
-    images = []
-    labelsss = []
-
-    for picClass in data_dict.values():
-        # picClass is a tuple (data, labels)
-        data = picClass[0] # array of pictures with dimensions [N, SIZE, SIZE]
-        labels = picClass[1] # array with dimensions [N, 1]
-        N = data.shape[0] # depends on number of pictures in class, number between [32, 50]
+    for classNum in dataDict.keys(): #iterates over all classes [1,101]
+        data = dataDict[classNum] # array of pictures with dimensions [N, SIZE, SIZE]
+        labels = labelDict[classNum] # array with dimensions [N, 1]
+        N = data.shape[0]  # depends on number of pictures in class, number between [32, 50]
 
         # print(f"Class pictures array size: {data.shape},\tclass label array size: {labels.shape}")
         tst_size = int(0.5 * N)  # this always outputs half of N, rounded down TODO: to put it outside the loop. it's override the N again and again.
         # print(f"Total class pictures: {N}\n\tTraining: {N - tst_size}\n\tTest: {tst_size}\n")
-        images.append(data)
-        labelsss.append(labels)
 
-        # for pic in X_train:
-        #     cv2.imshow("pic", pic)
-        #     cv2.waitKey(0)
+        images_list.append(data)
+        labels_list.append(labels)
 
-    X_train, X_test, y_train, y_test = train_test_split(images, labelsss, test_size=tst_size, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(images_list, labels_list, test_size=tst_size, shuffle=False)
     split_dict = {"Train": {'Data': X_train, 'Labels': y_train}, "Test": {'Data': X_test, 'Labels': y_test}}
 
     return split_dict
-
-
-def GetDefaultParameters():  # to add more parameters
-    class_indices = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-    image_size = (150, 150)
-    split = 0.2
-    clusters = 40
-    svm_c = 200
-    degree = 3
-    kernel = 'rbf'
-    gamma = 5
-    step_size = 6
-    bins = clusters
-    validate = False
-    parameters = {"class_indices": class_indices, "validate": validate, "image_size": image_size, "Split": split,
-                  "clusters": clusters, "step_size": step_size, "bins": bins, "svm_c": svm_c, "kernel": kernel,
-                  "gamma": gamma, 'degree': degree}
-    return parameters
-
-
-def train_kmeans(data, params):
-    print('Begin Kmeans training')
-
-    sift_vec = []  # define a list of sift
-    for img in data:
-        sift = cv2.xfeatures2d.SIFT_create()  # Creating Sifts
-        step_size = params['step_size']  # use the step size as defined in params
-        kp = [cv2.KeyPoint(x, y, step_size) for y in range(0, img.shape[0], step_size) for x in
-              range(0, img.shape[1], step_size)]  # compute key points
-        points, sifts = sift.compute(img, kp)  # computing the sifts from the keypoints.
-        sift_vec.append(sifts)  # sift_vec: array of all the sifts.
-
-    # transfer the list to np.array
-    all_sifts_array = list(sift_vec[0])
-    for value in sift_vec[1:]:
-        all_sifts_array = np.append(all_sifts_array, value, axis=0)
-    # compute and return k_means
-    model = MiniBatchKMeans(n_clusters=params["clusters"], random_state=42,
-                            batch_size=params['clusters'] * 4)  # Kmenas model parameters - TODO: need to check in hyperparameters tuning
-    kmeans = model.fit(all_sifts_array)  # Fitting the moddel on SIFT
-    print('Kmeans trained')
-
-    return kmeans
 
 
 def prepare(kmeans, data, params):
@@ -165,23 +156,48 @@ def prepare(kmeans, data, params):
     return histograms_vector
 
 
+def train_kmeans(data, trainParams):
+    print('Begin Kmeans training')
+
+    sift_vec = []  # define a list of sift
+    for img in data:
+        sift = cv2.xfeatures2d.SIFT_create()  # Creating Sifts
+        step_size = trainParams['step_size']  # use the step size as defined in params
+        kp = [cv2.KeyPoint(x, y, step_size) for y in range(0, img.shape[0], step_size) for x in
+              range(0, img.shape[1], step_size)]  # compute key points
+        points, sifts = sift.compute(img, kp)  # computing the sifts from the keypoints.
+        sift_vec.append(sifts)  # sift_vec: array of all the sifts.
+
+    # transfer the list to np.array
+    all_sifts_array = list(sift_vec[0])
+    for value in sift_vec[1:]:
+        all_sifts_array = np.append(all_sifts_array, value, axis=0)
+    # compute and return k_means
+    model = MiniBatchKMeans(n_clusters=trainParams["clusters"], random_state=42,
+                            batch_size=trainParams['clusters'] * 4)  # Kmenas model parameters - TODO: need to check in hyperparameters tuning
+    kmeans = model.fit(all_sifts_array)  # Fitting the moddel on SIFT
+    print('Kmeans trained')
+
+    return kmeans
+
+
+
 
 if __name__ == '__main__':
-    # data_path = r"C:\Users\Alina\OneDrive\Desktop\Studies\Learning, representation, and Computer Vision\Homework\Task 1\101_ObjectCategories"
-    # pickle_file_path = r'C:\Users\Alina\PycharmProjects\CVCourseT1\data.pkl'
-    data_path = r"C:\Users\razdo\Documents\_Dor\Second Degree documents\Courses\Semester 1\Learning, representation, and Computer Vision\Homework\Task 1 misc\101_ObjectCategories"
-    pickle_file_path = r"C:\Users\razdo\Documents\_Dor\Second Degree documents\Courses\Semester 1\Learning, representation, and Computer Vision\Homework\Task 1 misc\data.pkl"
 
-    # pickle_caltech101_images(data_path, pickle_file_path)
+    Params = GetDefaultParameters()
 
-    dataDict = load_data(pickle_file_path)
+    np.random.seed(0)
 
-    splitdict = create_sets(dataDict)
+    # pickle_caltech101_images(Params['Data'])
 
-    params = GetDefaultParameters()
+    DandL = GetData(Params["Data"])
 
-    kmeans_model = train_kmeans(splitdict['Train']['Data'], params)
+    SplitData = TrainTestSplit(DandL["Data"], DandL["Labels"], Params["Split"])
 
-    # TrainDataRep = prepare(kmeans_model, splitdict['Train']['Data'], params)
+    # TrainDataRep = prepare(SplitData["Train"]['Data'], Params["Prepare"])
+
+    # kmeans_model = train_kmeans(SplitData["Train"]['Data'], Params["Train"])
+
 
 
